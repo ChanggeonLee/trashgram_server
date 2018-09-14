7a048e0331d695b1e95372c3e755803e0eb0e00b
ChanggunLee <cd8114@gmail.com>
2018-09-15 00:24:05 +0900

    server set

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const info = [
+    {data: 'it so hard how con i do for study react-native'},
+    {data: 'it for test'},
+    {data: 'networking with node.js server'},    
+  ]
+  res.json(info)
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  console.log("POST POST");
+});
+
+
+module.exports = router;
