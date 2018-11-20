155afd32dbab59a04c2c9ebc01631aad3b18bcb7
ChanggunLee <cd8114@gmail.com>
2018-11-20 12:12:36 +0900

    add test

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
-  console.log("POST POST");
-  res.json();
+  const info = {data:"success"}
+  res.json(info);
 });
 module.exports = router;
