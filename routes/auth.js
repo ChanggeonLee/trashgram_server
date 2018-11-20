d47a02e61d2aa32ce577e86be8fc916697e9eae3
ChanggunLee <cd8114@gmail.com>
2018-11-20 14:07:34 +0900

    bug fix

--- routes/auth.js
+++ routes/auth.js
@@ -11,21 +11,21 @@ router.post('/', async(req, res, next) => {
   console.log(req.body);
   const info = {data:"success"}
 
-  var data = await request({
-    url: 'https://graph.facebook.com/v2.5/me?fields=email,name,picture',
-    method: 'GET',
-    qs: {
-        "access_token": req.body.token
-    },
-    }, function (error, response, body) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(body);
-  });
+  // var data = await request({
+  //   url: 'https://graph.facebook.com/v2.5/me?fields=email,name,picture',
+  //   method: 'GET',
+  //   qs: {
+  //       "access_token": req.body.token
+  //   },
+  //   }, function (error, response, body) {
+  //   res.setHeader('Content-Type', 'application/json');
+  //   res.send(body);
+  // });
 
 
-  var data = await request("");
+  var data = await request("https://graph.facebook.com/v2.5/me?fields=email,name,picture&access_token="+req.body.token);
   console.log(data);
-  
+
   res.json(info);
 });
 module.exports = router;
